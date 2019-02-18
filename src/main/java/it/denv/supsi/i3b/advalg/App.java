0e88c2a719a64fecc46f379c6e77c2b43e6b8fe0
Denys Vitali <denys@denv.it>
2019-02-18 13:46:36 +0100

    Initial Commit

---
+++ src/main/java/it/denv/supsi/i3b/advalg/App.java
@@ -0,0 +1,13 @@
+package it.denv.supsi.i3b.advalg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
