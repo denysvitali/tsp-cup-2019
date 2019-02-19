6e7a1668e48f9ee9cfc90a7e06f2db4740bceac7
Denys Vitali <denys@denv.it>
2019-02-19 10:00:21 +0100

    Improve parser

--- src/main/java/it/denv/supsi/i3b/advalg/algorithms/TSP/io/TSPLoader.java
+++ src/main/java/it/denv/supsi/i3b/advalg/algorithms/TSP/io/TSPLoader.java
@@ -1,7 +1,9 @@
 package it.denv.supsi.i3b.advalg.algorithms.TSP.io;
 
+import it.denv.supsi.i3b.advalg.algorithms.EdgeWeightType;
 import it.denv.supsi.i3b.advalg.algorithms.ProblemType;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -9,7 +11,7 @@ public class TSPLoader {
 	private String path;
 	private boolean loaded = false;
 	private TSPData tspData = null;
-	private FileInputStream fis = null;
+	private BufferedInputStream fis = null;
 
 	public TSPLoader(String filePath){
 		this.path = filePath;
@@ -17,7 +19,7 @@ public class TSPLoader {
 
 	public TSPData load() throws IOException {
 		if(!loaded){
-			fis = new FileInputStream(path);
+			fis = new BufferedInputStream(new FileInputStream(path));
 			tspData = parseFile();
 			loaded = true;
 		}
@@ -25,47 +27,83 @@ public class TSPLoader {
 		return tspData;
 	}
 
-	private int parseTag(String tag) throws IOException {
+	private void parseTag(String tag) throws IOException {
 		char[] type_tag = tag.toCharArray();
+		fis.mark(1);
 		int c = fis.read();
 		for (char value : type_tag) {
 			assert (c == value);
+			fis.mark(1);
 			c = fis.read();
 		}
 
-		return c;
+		fis.reset();
 	}
 
 	private TSPData parseFile() throws IOException {
 		TSPData data = new TSPData();
-		int c = 0;
+
+		int c = -2;
 
 		// NAME Parsing
-
+		parseTag("NAME: ");
 		data.name = getString(fis);
 		assert(fis.read() == '\n');
 
 		// TYPE Parsing
-		c = parseTag("TYPE: ");
+		parseTag("TYPE: ");
 		data.type = ProblemType.valueOf(getString(fis));
-		assert(c == '\n');
+		assert(fis.read() == '\n');
 
 		// COMMENT parsing
-		c = parseTag("COMMENT: ");
+		parseTag("COMMENT: ");
 		data.comment = getString(fis);
+		assert(fis.read() == '\n');
+
+		parseTag("DIMENSION: ");
+		data.dimension = getInt(fis);
+		assert(fis.read() == '\n');
+
+		parseTag("EDGE_WEIGHT_TYPE: ");
+		data.ewt = EdgeWeightType.valueOf(
+				getString(fis)
+		);
+		assert(fis.read() == '\n');
+
+		parseTag("BEST_KNOWN :");
+
 
 
 		return data;
 	}
 
-	private String getString(FileInputStream fis) throws IOException {
+	private String getString(BufferedInputStream fis) throws IOException {
 		StringBuilder sb = new StringBuilder();
+		fis.mark(1);
 		int c = fis.read();
 		while(c != '\n' && c != -1){
 			sb.append((char) c);
+			fis.mark(1);
 			c = fis.read();
 		}
-
+		fis.reset();
 		return sb.toString();
+	}
+
+	private static boolean isNumeric(int c){
+		return c >= '0' && c <= '9';
+	}
+
+	private int getInt(BufferedInputStream fis) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		fis.mark(1);
+		int c = fis.read();
+		while(isNumeric(c)) {
+			sb.append((char) c);
+			fis.mark(1);
+			c = fis.read();
+		}
+		fis.reset();
+		return Integer.valueOf(sb.toString());
 	}
 }
